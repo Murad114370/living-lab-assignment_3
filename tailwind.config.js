73136767efeee31e1ee15268939952aeaca8e3c1
Muradul Islam <1053murad@gmail.com>
2025-02-17 19:56:48 +0600

    added animated nav section & set up responsivness (quantity ,services)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  theme: {
+    extend: {
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        'slide-down': {
+          '0%': { transform: 'translateY(-50px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        'zoom-in': {
+          '0%': { transform: 'scale(0.8)', opacity: '0' },
+          '100%': { transform: 'scale(1)', opacity: '1' },
+        },
+      },
+      animation: {
+        'fade-in': 'fade-in 1.2s ease-out',
+        'slide-down': 'slide-down 1s ease-in-out',
+        'zoom-in': 'zoom-in 1.5s ease-in-out',
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
